1358b7c3091360285a3c29c51fcfab0ea6742c81
olyaiy <alexolyaiy@gmail.com>
2024-07-05 20:39:52 -0700

    add styles.css and style components

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,18 @@ import {useCallback, useRef} from 'react'
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+  ];
+  
 export default function TextEditor() {
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return
@@ -9,11 +21,11 @@ export default function TextEditor() {
         wrapper.innerHTML = ''
         const editor = document.createElement("div");
         wrapper.append(editor)
-        new Quill(editor, { theme: "snow", });
+        new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS }});
 
     }, [])
 
   return (
-    <div id="container" ref={wrapperRef}></div>
+    <div className="container" ref={wrapperRef}></div>
   )
 }
