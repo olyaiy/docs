3da8c8eb621f17fc9a049ba9807d353c48e6a020
olyaiy <alexolyaiy@gmail.com>
2024-07-05 21:28:10 -0700

    handle multiple documents

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,7 @@ import {useCallback, useRef, useEffect, useState} from 'react'
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io }  from 'socket.io-client'
+import { useParams } from 'react-router-dom';
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,6 +17,7 @@ const TOOLBAR_OPTIONS = [
   ];
   
 export default function TextEditor() {
+  const {id: documentId} = useParams()
   const [socket, setSocket] = useState()
   const [quill, setQuill] = useState()
 
@@ -29,6 +31,18 @@ export default function TextEditor() {
   }, [])
 
   useEffect(() => {
+    if (socket == null || quill == null) return
+
+    socket.once('load-document', document => {  
+      quill.setContents(document)
+      quill.enable()
+    })
+
+    socket.emit('get-document', documentId)
+
+  }, [socket, quill, documentId])
+
+  useEffect(() => {
     if(quill == null || socket == null) return
     
     const handler = (delta) => {
@@ -40,9 +54,6 @@ export default function TextEditor() {
         quill.off('receive-changes', handler)
     }
   }, [socket, quill])
-
-
-
 
   useEffect(() => {
     if(quill == null || socket == null) return
@@ -66,6 +77,8 @@ export default function TextEditor() {
         const editor = document.createElement("div");
         wrapper.append(editor)
         const q = new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS }});
+        q.disable()
+        q.setText('Loading...')
         setQuill(q)
     }, [])
 
